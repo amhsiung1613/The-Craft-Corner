9336559c02d46da6fb616b59be05061c8cfa8376
amhsiung1613 <133808504+amhsiung1613@users.noreply.github.com>
2026-02-23 22:50:58 -0800

    trying to convert to next.js, revert to previous commit if necessary

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,39 @@
-export const metadata = {
-  title: 'The Craft Corner',
-  description: 'Web site created with Next.js.',
-}
+// export const metadata = {
+//   title: 'The Craft Corner',
+//   description: 'Web site created with Next.js.',
+// }
  
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//         <body>
+//             <div id="root">{children}</div>
+//         </body>
+//     </html>
+//   )
+// }
+
+"use client";
+
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { ShopContextProvider } from "../context/shop-context";
+import { FavContextProvider } from "../context/fav-context";
+import "../css/index.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <body>
-            <div id="root">{children}</div>
-        </body>
+      <body>
+        <ShopContextProvider>
+          <FavContextProvider>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+          </FavContextProvider>
+        </ShopContextProvider>
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+import Home from "../pages/Home"; // temporary until you move it
+
+export default function Page() {
+  return <Home />;
+}
