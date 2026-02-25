818e913e3dbcee2a6e0aaf510bc667868837d013
amhsiung1613 <133808504+amhsiung1613@users.noreply.github.com>
2026-02-24 18:29:11 -0800

    another possible fix?

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,7 +1,7 @@
-"use client";
+// export const dynamic = "force-dynamic";
 
 import Display from "../../components/display"; // temporary until you move it
 
-export default function ProductsPage() {
-  return <Display />;
+export default function ProductsPage({ searchParams }) {
+  return <Display initialCategory={searchParams.category} />;
 }
