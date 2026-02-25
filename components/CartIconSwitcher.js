d7160253a7ad5e9e3a37a98e903270a936dcb9f9
amhsiung1613 <133808504+amhsiung1613@users.noreply.github.com>
2026-02-24 22:33:28 -0800

    moved some stuff to public, deleted old package lock, still confused

---
+++ components/CartIconSwitcher.js
@@ -0,0 +1,32 @@
+import React, { useState, useContext } from 'react';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import IconButton from '@mui/material/IconButton';
+import { ShopContext } from '../src/context/shop-contextext';
+
+function IconSwitcher({ productId }) {
+  const [isInCart, setIsInCart] = useState(false);
+  const { addToCart, removeFromCart, cartItems } = useContext(ShopContext);
+
+  // Check if the product is in the cart initially
+  React.useEffect(() => {
+    setIsInCart(!!cartItems[productId]);
+  }, [cartItems, productId]);
+
+  const toggleIcon = () => {
+    if (isInCart) {
+      removeFromCart(productId);
+    } else {
+      addToCart(productId);
+    }
+    setIsInCart(prevState => !prevState);
+  };
+
+  return (
+    <IconButton onClick={toggleIcon}>
+      {isInCart ? <ShoppingCartIcon /> : <AddShoppingCartIcon />}
+    </IconButton>
+  );
+  }
+
+export default IconSwitcher;
---
+++ components/FavIconSwitcher.js
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { useState, useContext } from 'react';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import IconButton from '@mui/material/IconButton';
+import { FavContext } from '../src/context/fav-contextext';
+
+function IconSwitcher({ productId }) {
+  // const [icon, setIcon] = useState('full');
+  const [ isInFav, setIsInFav ] = useState(false);
+  const { addToFav, removeFromFav, favItems } = useContext(FavContext);
+
+  React.useEffect(() => {
+    setIsInFav(!!favItems[productId]);
+  }, [favItems, productId]);
+
+  const toggleIcon = () => {
+    if (isInFav) {
+      removeFromFav(productId);
+    } else {
+      addToFav(productId);
+    }
+    setIsInFav(prevState => !prevState);
+  };
+
+  return (
+    <IconButton onClick={toggleIcon}>
+      {isInFav ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+    </IconButton>
+  );
+}
+
+export default IconSwitcher
