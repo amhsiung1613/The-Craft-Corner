6acd258de152191656744a04ae50da8fde59424f
amhsiung1613 <133808504+amhsiung1613@users.noreply.github.com>
2026-02-25 18:57:41 -0800

    finalizing carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,11 +2,13 @@
 import React from "react";
 import news1 from "../assets/carousel-1.jpg";
 import news2 from "../assets/carousel-2.jpg";
+import news3 from "../assets/pearls.jpg";
 import {ImageSlider} from "./carousel-new/ImageSlider.tsx";
 
 const IMAGES = [
   { id: 1, url: news1.src, alt: "News One" },
   { id: 2, url: news2.src, alt: "News Two" },
+  { id: 3, url: news3.src, alt: "News Three" },
 ]
 
 function Carousel() {
