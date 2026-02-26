897faeefabaeaf8a1bb234a8a05ce754676fe8a6
amhsiung1613 <133808504+amhsiung1613@users.noreply.github.com>
2026-02-26 12:40:33 -0800

    preventing pre-render of checkout success page

--- src/app/checkout-success/page.js
+++ src/app/checkout-success/page.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import CheckoutSuccess from "../../components/Checkout-Success"; // temporary until you move it
 
 export default function CheckoutSuccessPage() {
