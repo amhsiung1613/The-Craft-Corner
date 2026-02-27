3a179ddc98fc768c16c831aa9bca4f24e051cf81
amhsiung1613 <133808504+amhsiung1613@users.noreply.github.com>
2026-02-26 20:55:38 -0800

    trying favitem

--- src/components/Checkout-Success.js
+++ src/components/Checkout-Success.js
@@ -1,8 +1,9 @@
 "use client";
 
 import React, { useContext } from "react";
+import { FavItem } from "./fav-item";
 // import { CartItem } from "./cart-item";
-import { ProductCard } from "../components/ProductCard";
+// import { ProductCard } from "../components/ProductCard";
 import ProductList from "../inventory/ProductList"
 import styles from "../css/CheckoutSuccess.module.css";
 import { useRouter } from "next/navigation";
@@ -22,7 +23,7 @@ function CheckoutSuccess() {
                 {ProductList.map((product) => {
                     if (cartItems[product.id] !== 0) {
                         return (
-                        <ProductCard
+                        <FavItem
                             key={product.id}
                             data={product}
                         />
