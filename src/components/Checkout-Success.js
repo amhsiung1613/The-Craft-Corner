e55b4f7a540029544f694ed790623b004f59ee89
amhsiung1613 <133808504+amhsiung1613@users.noreply.github.com>
2026-02-26 20:47:04 -0800

    trying different component

--- src/components/Checkout-Success.js
+++ src/components/Checkout-Success.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useContext } from "react";
-// import { CartItem } from "./cart-item";
+import { CartItem } from "./cart-item";
 import ProductCard from "../components/ProductCard";
 import ProductList from "../inventory/ProductList"
 import styles from "../css/CheckoutSuccess.module.css";
@@ -22,7 +22,7 @@ function CheckoutSuccess() {
                 {ProductList.map((product) => {
                     if (cartItems[product.id] !== 0) {
                         return (
-                        <ProductCard
+                        <CartItem
                             key={product.id}
                             data={product}
                         />
