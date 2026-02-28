52817abf5e1318aec6c49f5dd442df4419a18676
amhsiung1613 <133808504+amhsiung1613@users.noreply.github.com>
2026-02-28 13:14:27 -0800

    undid breadcrumb css

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -8,7 +8,7 @@ const BreadCrumb = ({ pages = [{ name: '', link: '' }] }) => {
 // Going through a loop to construct the list item of the name and link from the array of objects passed in.
     let breadCrumbs = pages.map((page, index) => {
 // If name and link is present in the data.
-        if (page.link) return <li key={ index } className={styles.breadcrumb-item}>
+        if (page.link) return <li key={ index } className="breadcrumb-item">
             <Link href={ page.link }> { page.name }</Link></li>
 // If just a name is present in the data.
         return <li key={ index } className="breadcrumb-item active" aria-current="page">{ page.name }</li>
@@ -17,8 +17,8 @@ const BreadCrumb = ({ pages = [{ name: '', link: '' }] }) => {
     return (
         <>
             <nav aria-label="breadcrumb">
-                <ol className={styles.breadcrumb}>
-                    <li className={styles.breadcrumb-item}><Link href="/products">Products</Link></li>
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><Link href="/products">Products</Link></li>
                     { breadCrumbs }
                 </ol>
             </nav>
