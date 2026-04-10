cd11089bd3b2c8547c716cfca5f3a5a0c1b54fc5
Amber <amberhsiung@gmail.com>
2026-04-10 16:08:55 -0700

    changed css for carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,6 +4,7 @@ import news1 from "../assets/carousel-1.jpg";
 import news2 from "../assets/carousel-2.jpg";
 import news3 from "../assets/pearls.jpg";
 import {ImageSlider} from "./carousel-new/ImageSlider.tsx";
+import styles from "../css/Carousel.module.css";
 
 const IMAGES = [
   { id: 1, url: news1.src, alt: "News One" },
@@ -13,16 +14,7 @@ const IMAGES = [
 
 function Carousel() {
   return (
-    <div
-      style={{
-        maxWidth: "1550px",
-        width: "100%",
-        maxHeight: "400px",
-        height: "100%",
-        aspectRatio: "10 / 6",
-        margin: "0 auto",
-      }}
-    >
+    <div className={styles.carousel}    >
       <ImageSlider images={IMAGES} />
       {/* <a href="/" style={{ fontSize: "4rem" }}>
         Link
