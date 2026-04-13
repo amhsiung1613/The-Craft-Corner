8ad44d38b8496be6b92e967c997f91d9999980ca
Amber <amberhsiung@gmail.com>
2026-04-13 13:02:11 -0700

    adding cart and fav to navbar on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 // import { useRouter } from "next/router";
 import ReorderIcon from '@mui/icons-material/Reorder';
-// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+// import IconButton from '@mui/material/IconButton';
 import styles from "../css/Navbar.module.css";
 // import { ShopContext } from '../context/shop-context';
 
@@ -27,11 +29,11 @@ function Navbar() {
             <Image src={Logo.src} alt="Logo" width={80} height={80} style={{ cursor: 'pointer'}}/>
           </Link>
           <div className={styles.hiddenLinks}>
-            <Link href="/home"> Home </Link>
+            {/* <Link href="/home"> Home </Link> */}
             <Link href="/products"> Products </Link>
             {/* <Link to="/search"> Search </Link> */}
-            <Link href="/favorites"> Favorites </Link>
-            <Link href="/cart" > Cart </Link> 
+            {/* <Link href="/favorites"> Favorites </Link>
+            <Link href="/cart" > Cart </Link>  */}
             {/* <Link to="/account"> Account </Link> */}
             <Link href="/about"> About </Link>
           </div>
@@ -44,6 +46,12 @@ function Navbar() {
           {/* <Link to="/account"> Account </Link> */}
           <Link href="/about"> About </Link>
           
+          <a href="/cart">
+            <ShoppingCartIcon />
+          </a>
+          <a href="/favorites">
+            <FavoriteIcon />
+          </a>
           <button onClick={toggleNavbar}>
             <ReorderIcon />
           </button>
